12e87b18613ac58c8af1c494dafad5e98a01bebf
Chan Chun Yu <itroychan@gmail.com>
2022-07-25 11:11:58 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import './App.scss';
-import Challenge from './pages/Challenge';
-
 import Index from './pages/Index.jsx';
 function App() {
-  return <Challenge></Challenge>;
+  return <Index></Index>;
 }
 
 export default App;
